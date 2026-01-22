7f8d90683d5ad25de608ee37f2e4bb65ff9eefd4
andersontamiozzo <114349983+andersontamiozzo@users.noreply.github.com>
2026-01-22 00:03:31 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-// Service Worker Vazio
+// Service Worker Mínimo Funcional
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  // Deixa as requisições passarem normalmente
+  event.respondWith(fetch(event.request));
+});
